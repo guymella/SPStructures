e45c6f588f7b60b224420a6c323f56a390cde558
guymella <guymella@github.com>
2019-01-08 13:54:29 -0700

    fipsified skeleton module - unit tests building and running.

---
+++ src/Structures/Block.h
@@ -0,0 +1,13 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Structures::Block
+    @ingroup Block
+    @brief Block of Memory
+*/
+
+class Block {
+public:
+	Block() {};
+};
+
---
+++ src/Structures/Interfaces/iBlock.h
@@ -0,0 +1,12 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class Structures::Interfaces::iBlock
+    @ingroup Interfaces
+    @brief interface for memory blocks
+*/
+
+	class iBlock {
+	public:
+		iBlock() {};
+	};
